7f44c93ed6f1f44dd6c2b27da7afabedd46ccaa9
NolanCST <nolan.costa06@gmail.com>
2023-10-12 14:26:29 +0200

    Correction beug

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -28,27 +28,6 @@ function NavBar() {
             <nav>
                <Link to="/ProfilPage">Profil</Link>
             </nav>
-         </div>
-      </>
-   );
-   return (
-      <>
-         <div>
-            <nav>
-               <Link to="/">Aller à l'accueil</Link>
-            </nav>
-            <nav>
-               <Link to="/PlaySation">PlaySation</Link>
-            </nav>
-            <nav>
-               <Link to="/Xbox">Xbox</Link>
-            </nav>
-            <nav>
-               <Link to="/Switch">Switch</Link>
-            </nav>
-            <nav>
-               <Link to="/Computer">Computer</Link>
-            </nav>
             <nav>
                <Link to="/POSTPosts">Creer un Post</Link>
             </nav>
