81f026bb9b3d9771fd71c1a497e7d7981e61dc50
Zaïnab <zeinab0695@gmail.com>
2023-10-12 16:53:28 +0200

    Footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,11 +4,6 @@ function Footer() {
   return (
     <footer>
       <p>&copy; 2023 GamerVerse FR, Inc. All rights reserved.</p>
-      <div className="span">
-        <span>Privacy Policy</span>
-        <span>Terms of Use</span>
-        <span>Accessibility</span>
-      </div>
     </footer>
   );
 }
