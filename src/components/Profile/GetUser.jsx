3064b49dc28c6c18acd26109ada626233e56c454
Dennys5 <dennys.rocca07@gmail.com>
2023-10-13 12:03:21 +0200

    Modification components

--- src/components/Profile/GetUser.jsx
+++ src/components/Profile/GetUser.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import PutUser from "./PutUser";
 
 function GetUser() {
+    
     const [user,setUser] = useState([]);
 
     const getUser = async () => {
@@ -11,19 +13,45 @@ function GetUser() {
             "Authorization": "bearer " + localStorage.getItem("@TokenUser"),
            },
         };
-        try {
-           const response = await fetch("https://social-network-api.osc-fr1.scalingo.io/gamer-verse/Profil?limit=20", options);
-           if (response.ok) {
+           const response = await fetch("https://social-network-api.osc-fr1.scalingo.io/gamer-verse/profil?limit=20", options);
               const data = await response.json();
+
               console.log(data);
+
               setUser(data);
-           } else {
-              console.error("Échec de la requête HTTP");
-           }
-        } catch (error) {
-           console.error("Erreur lors de la requête : ", error);
-        }
      };
+
+     return(
+        <>
+        <div className="profilInput">
+
+            <h1 className="title">Profil</h1>
+
+            <div className="choix">
+                <select name="Plateforme" className="platform">
+                    <option value="Plateforme">Plateforme</option>
+                    <option value="playstation">Playstation</option>
+                    <option value="Xbox">Xbox</option>
+                    <option value="switch">Switch</option>
+                    <option value="Pc">PC</option>
+                </select>
+            </div>
+
+            <div className="identity">
+                <input type="text" name="firstName" className="firstName" placeholder="Nom"/>
+                <input type="text" name="lastName" className="lastName" placeholder="Prénom"/>
+                <input type="number" name="age" className="age" placeholder="Âge" />
+            </div>
+
+            <div className="mailInput">
+                <input type="text" name="email" className="email" placeholder="E-mail"/>
+                <input type="password" name="passewordChange" className="Mdp" placeholder="Mot de passe"/>
+            </div>
+
+            <button className="switchMdp">Modifier Profil</button>
+        </div>
+        </>
+     )
 }
 
 export default GetUser;
--- src/components/Profile/Profil.jsx
+++ src/components/Profile/Profil.jsx
@@ -9,38 +9,9 @@ function ProfilPage (){
 
 return(
     <div className="Background">
-
-    <NavBar/>
-
-    <div className="profilInput">
-
-        <h1 className="title">Profil</h1>
-
-        <div className="choise">
-            <select name="Plateforme" className="platform">
-                <option value="Plateforme">Plateforme</option>
-                <option value="playstation">Playstation</option>
-                <option value="Xbox">Xbox</option>
-                <option value="switch">Switch</option>
-                <option value="Pc">PC</option>
-            </select>
-        </div>
-
-        <div className="identity">
-            <input type="text" name="firstName" className="firstName" placeholder="Nom"/>
-            <input type="text" name="lastName" className="lastName" placeholder="Prénom"/>
-            <input type="number" name="age" className="age" placeholder="Âge" />
-        </div>
-    
-        <div className="mailInput">
-            <input type="text" name="email" className="email" placeholder="E-mail"/>
-            <input type="password" name="passewordChange" className="Mdp" placeholder="Mot de passe"/>
-        </div>
-        
-        <button className="switchMdp">Modifier Profil</button>
-    </div>
-
-    <Footer/>
+        <NavBar/>
+            <GetUser/>
+        <Footer/>
     </div>
 )
 }
