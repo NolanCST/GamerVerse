641d2b7d27cedf1820e6a037de997eaf2a092983
NolanCST <nolan.costa06@gmail.com>
2023-10-16 11:29:10 +0200

    petit beug corrige

--- src/components/posts/RenderPosts.jsx
+++ src/components/posts/RenderPosts.jsx
@@ -55,42 +55,5 @@ function RenderPosts(props) {
          </Card>
       </>
    );
-   return (
-      <>
-         <div className="infosPosts"></div>
-         <Card sx={{ margin: 5 }}>
-            <CardHeader
-               avatar={
-                  <Avatar sx={{ bgcolor: "purple" }} aria-label="recipe">
-                     As
-                  </Avatar>
-               }
-               action={
-                  <IconButton aria-label="settings">
-                     <MoreVert />
-                  </IconButton>
-               }
-               title={props.title}
-               subheader={props.date}
-            />
-            <CardContent>
-               <Typography variant="body2" color="text.secondary">
-                  {props.content}
-               </Typography>
-            </CardContent>
-            <CardMedia component="img" height="240" image="https://www.tryagame.fr/wp-content/uploads/2023/05/1676385974-1953-capture-d-ecran.jpg" alt="Zelda tokt" />
-
-            <CardActions disableSpacing>
-               <IconButton n aria-label="add to favorites">
-                  <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: "#ff1744" }} />} />
-                  {props.like}
-               </IconButton>
-               <IconButton aria-label="share">
-                  <Share />
-               </IconButton>
-            </CardActions>
-         </Card>
-      </>
-   );
 }
 export default RenderPosts;
