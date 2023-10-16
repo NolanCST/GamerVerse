dc1a2d0165289e954de8bb69db6e916461910e38
vsantidev <vsantidev@gmail.com>
2023-10-16 09:22:21 +0200

    modif rifht bar + test grid saloon & home

--- src/components/home/RenderHome.jsx
+++ src/components/home/RenderHome.jsx
@@ -3,7 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Box, Stack, Divider } from "@mui/material";
+import { CardActionArea, Box, Stack, Divider, Chip } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -11,89 +11,160 @@ import { Link } from "react-router-dom";
 import "./RenderHome.css";
 
 const Item = styled(Paper)(({ theme }) => ({
-   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-   ...theme.typography.body2,
-   padding: theme.spacing(1),
-   textAlign: "center",
-   color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
 }));
 
 function RenderHome() {
-   return (
-      
-      <div className="cardContainer">
-         <Stack direction="column" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
-         <Box sx={{ width: "100%" }}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-               <Grid className="testOne" display="flex" justifyContent="center" item xs={6}>
-                  <Card sx={{ maxWidth: 345 }}>
-                     <Link to="/PlayStation" style={{ textDecoration: "none", color: "Black" }}>
-                        <CardActionArea>
-                           <CardMedia component="img" image="/images/playstation.jpg" alt="PlayStation" />
-                           <CardContent>
-                              <Typography gutterBottom variant="h5" component="div">
-                                 Modérateur : Dennys
-                              </Typography>
-                              <Typography variant="body2" >
-                              </Typography>
-                           </CardContent>
-                        </CardActionArea>
-                     </Link>
-                  </Card>
-               </Grid>
-               <Grid display="flex" justifyContent="center" item xs={6}>
-                  <Card sx={{ maxWidth: 345 }}>
-                     <Link to="/Xbox" style={{ textDecoration: "none", color: "Black" }}>
-                        <CardActionArea>
-                           <CardMedia component="img" image="/images/Xbox.jpg" alt="Xbox" />
-                           <CardContent>
-                              <Typography gutterBottom variant="h5" component="div">
-                              Modératrice : Zaïnab
-                              </Typography>
-                              <Typography variant="body2" color="text.secondary">
-                              </Typography>
-                           </CardContent>
-                        </CardActionArea>
-                     </Link>
-                  </Card>
-               </Grid>
-               <Grid display="flex" justifyContent="center" item xs={6}>
-                  <Card sx={{ maxWidth: 345 }}>
-                     <Link to="/Switch" style={{ textDecoration: "none", color: "Black" }}>
-                        <CardActionArea>
-                           <CardMedia component="img" image="/images/Switch.jpg" alt="Switch" />
-                           <CardContent>
-                              <Typography gutterBottom variant="h5" component="div">
-                                 Modératrice : Aslinn
-                              </Typography>
-                              <Typography variant="body2" color="text.secondary">
-                              </Typography>
-                           </CardContent>
-                        </CardActionArea>
-                     </Link>
-                  </Card>
-               </Grid>
-               <Grid display="flex" justifyContent="center" item xs={6}>
-                  <Card sx={{ maxWidth: 345 }}>
-                     <Link to="/Computer" style={{ textDecoration: "none", color: "Black" }}>
-                        <CardActionArea>
-                           <CardMedia component="img" image="/images/PC.jpg" alt="Computer" />
-                           <CardContent>
-                              <Typography gutterBottom variant="h5" component="div">
-                              Modérateur : Nono
-                              </Typography>
-                              <Typography variant="body2" color="text.secondary">
-                              </Typography>
-                           </CardContent>
-                        </CardActionArea>
-                     </Link>
-                  </Card>
-               </Grid>
-            </Grid>
-         </Box>
-         </Stack>
-      </div>
-   );
+  return (
+    <div className="cardContainer">
+      <Stack
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+        spacing={2}
+        useFlexGap
+        flexWrap="wrap"
+      >
+        {/* <Box sx={{ width: "100%" }}> */}
+
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid
+            className="testOne"
+            display="flex"
+            justifyContent="space-around"
+            item
+            xs={12}
+            md={3}
+          >
+            <Card sx={{ maxWidth: 300 }}>
+              <CardActionArea>
+                <Link
+                  to="/PlayStation"
+                  style={{ textDecoration: "none", color: "Black" }}
+                >
+                  <CardMedia
+                    component="img"
+                    image="/images/playstation.jpg"
+                    alt="PlayStation"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      "PLAYS HAS NO LIMITS"
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Modérateur : Dennys date dernier post nbre de post
+                    </Typography>
+                  </CardContent>
+                  <Chip label="Final Fantasy 16" color="primary"/>
+                  
+                </Link>
+              </CardActionArea>
+            </Card>
+          </Grid>
+
+          <Grid
+            display="flex"
+            justifyContent="space-around"
+            item
+            xs={12}
+            md={3}
+          >
+            <Card sx={{ maxWidth: 300 }}>
+              <Link
+                to="/Xbox"
+                style={{ textDecoration: "none", color: "Black" }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image="/images/Xbox.jpg"
+                    alt="Xbox"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      "POWER YOUR DREAMS"
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Modératrice : Zaïnab
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
+            </Card>
+          </Grid>
+
+          <Grid
+            display="flex"
+            justifyContent="space-around"
+            item
+            xs={12}
+            md={3}
+          >
+            <Card sx={{ maxWidth: 300 }}>
+              <Link
+                to="/Switch"
+                style={{ textDecoration: "none", color: "Black" }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image="/images/Switch.jpg"
+                    alt="Switch"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      "PLAY TOGETHER ANYTIME, ANYWHERE"
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Modératrice : Aslinn
+                    </Typography>
+                  </CardContent>
+                  <Chip label="Hey ! Listen" style={{color:"red"}}/>
+                  <Chip label="Zelda Botw / Zeldat tokt" sx={{bgcolor:"red", color:"white"}}/>
+                </CardActionArea>
+              </Link>
+            </Card>
+          </Grid>
+
+          <Grid
+            display="flex"
+            justifyContent="space-around"
+            item
+            xs={12}
+            md={3}
+          >
+            <Card sx={{ maxWidth: 300, boxShadow: 3 }}>
+              <Link
+                to="/Computer"
+                style={{ textDecoration: "none", color: "Black" }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image="/images/PC.jpg"
+                    alt="Computer"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      "THE WAY IT'S MEANT TO BE PLAYED"
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Modérateur : Nono (Wario the winner !)
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
+            </Card>
+          </Grid>
+        </Grid>
+        {/* </Box> */}
+      </Stack>
+    </div>
+  );
 }
 
 export default RenderHome;
--- src/components/saloon/Switch.jsx
+++ src/components/saloon/Switch.jsx
@@ -3,13 +3,17 @@ import { Box, Stack } from "@mui/material";
 import Rightbar from "../layouts/Rightbar";
 import NavBar from "../layouts/NavBar";
 import Posts from "../posts/Posts";
+import Leftbar from "../layouts/Leftbar";
 
 export default function Switch() {
   return (
     <>
       <NavBar />
-      <Stack direction="row" spacing={2}>
-      <Box flex={4} p={2}>
+
+      <Stack direction="row" spacing={0}
+   useFlexGap flexWrap="wrap">
+          {/* <Leftbar /> */}
+      <Box flex={5} p={2} mr={20} ml={20}>
         <Posts />
       </Box>
       <Rightbar />
