0f4990e5044f6dce6b6c223bc166c21323e1f18e
Zaïnab <zeinab0695@gmail.com>
2023-10-16 16:42:58 +0200

    Age

--- src/components/Profile/GetUser.jsx
+++ src/components/Profile/GetUser.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function GetUser() {
   const [user, setUser] = useState({});
-  const [showPassword, setShowPassword] = useState(false);
+  const [newfirstName, setNewFirstName] = useState("");
+  const [newLastName, setNewLastName] = useState("");
+  const [editing, setEditing] = useState(false);
 
   const getUser = async () => {
     let options = {
@@ -40,30 +43,84 @@ function GetUser() {
     getUser();
   }, []);
 
+  const ModifProfil = async () => {
+    let options = {
+      method: "PUT",
+
+      Headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "bearer token " /* + localStorage.getItem("@TokenUser"), */,
+      },
+
+      Body: JSON.parse({
+        firstname: String,
+        lastname: String,
+        email: String,
+        age: Number,
+        occupation: String,
+      }),
+    };
+
+    const response = await fetch(
+      "https://social-network-api.osc-fr1.scalingo.io/gamer-verse/user",
+      options
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    setUser(data);
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+  };
+
+  useEffect(() => {
+    console.log("TEST 2");
+    ModifProfil();
+  }, []);
+
+  console.log("user :", user);
+
   return (
     <div className="profilInput">
       <h1 className="title">Profil</h1>
 
-      <div className="choix">
-        <select name="Plateforme" className="platform">
-          <option value="Plateforme">Plateforme</option>
-          <option value="playstation">Playstation</option>
-          <option value="Xbox">Xbox</option>
-          <option value="switch">Switch</option>
-          <option value="Pc">PC</option>
-        </select>
-      </div>
+      {editing ? ( // Si l'édition est activée
+        <div className="identity">
+          <div className="getFirstName">
+            Nom:{" "}
+            <input
+              type="text"
+              value={user.firstname}
+              onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+            />
+          </div>
 
-      <div className="identity">
-        <div className="getFirstName">Nom: {user.firstname}</div>
+          <div className="getLastName">
+            Prénom:{" "}
+            <input
+              type="text"
+              value={user.lastname}
+              onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+            />
+          </div>
 
-        <div className="getLastName">Prénom : {user.lastname}</div>
-
-        <div className="getAge">Age : {user.age}</div>
-      </div>
+          <button onClick={handleSave}>Sauvegarder</button>
+        </div>
+      ) : (
+        <div className="identity">
+          <div className="getFirstName">Nom: {user.firstname}</div>
+          <div className="getLastName">Prénom: {user.lastname}</div>
+          <button onClick={() => setEditing(true)}>Modifier</button>
+        </div>
+      )}
 
       <div className="mailInput">
-        <div className="getEmail">Mail :{user.email}</div>
+        <div className="getEmail">Mail: {user.email}</div>
       </div>
     </div>
   );
