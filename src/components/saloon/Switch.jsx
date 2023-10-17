ccb62f71cbc609b6543c26b04e4c6bca3526d1e2
NolanCST <nolan.costa06@gmail.com>
2023-10-17 16:49:08 +0200

    Rajout Leftbar

--- src/components/saloon/Switch.jsx
+++ src/components/saloon/Switch.jsx
@@ -3,6 +3,7 @@ import NavBar from "../layouts/NavBar";
 import Posts from "../posts/Posts";
 import Footer from "../layouts/Footer";
 import Rightbar from "../layouts/Rightbar";
+import Leftbar from "../layouts/Leftbar";
 
 export default function Switch() {
    return (
@@ -10,6 +11,7 @@ export default function Switch() {
          <NavBar />
          <div className="main-post-title">SWITCH</div>
          <Stack spacing={{ xs: 6, sm: 2 }} direction="row" flexWrap="wrap" justify-content="space-between" rowspacing={1} columnpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Leftbar />
             <Grid item xs={2} sm={4} md={4}>
                <Posts />
             </Grid>
