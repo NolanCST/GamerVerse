179f2f84b79cebb77aad9167697312cfc0161859
Zaïnab <zeinab0695@gmail.com>
2023-10-18 16:34:20 +0200

    Media queries, responsive dans les saloons

--- src/components/saloon/Switch.jsx
+++ src/components/saloon/Switch.jsx
@@ -5,19 +5,28 @@ import Footer from "../layouts/Footer";
 import Rightbar from "../layouts/Rightbar";
 import Leftbar from "../layouts/Leftbar";
 import Add from "../posts/actions/Add";
+import "./Playstation.css";
 
 export default function Switch() {
-   return (
-      <>
-         <NavBar />
-         <div className="main-post-title">SWITCH</div>
-         <Stack spacing={{ xs: 6, sm: 2 }} direction="row" flexWrap="wrap" justify-content="space-between" rowspacing={1} columnpacing={{ xs: 1, sm: 2, md: 3 }} marginBottom={10}>
-            <Leftbar />
-            <Posts type="Switch" />
-            <Rightbar />
-         </Stack>
-         <Add />
-         <Footer />
-      </>
-   );
+  return (
+    <>
+      <NavBar />
+      <div className="main-post-title">SWITCH</div>
+      <Stack
+        spacing={{ xs: 6, sm: 2 }}
+        direction="row"
+        flexWrap="wrap"
+        justify-content="space-between"
+        rowspacing={1}
+        columnpacing={{ xs: 1, sm: 2, md: 3 }}
+        marginBottom={10}
+      >
+        <Leftbar />
+        <Posts type="Switch" />
+        <Rightbar />
+      </Stack>
+      <Add />
+      <Footer />
+    </>
+  );
 }
