70149276d5896b2ee4e421763c071ace0d220d23
Zaïnab <zeinab0695@gmail.com>
2023-10-18 14:26:24 +0200

    Login responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,53 +5,53 @@ import Home from "./components/home/Home";
 import Xbox from "./components/saloon/Xbox";
 import Switch from "./components/saloon/Switch";
 import Computer from "./components/saloon/Computer";
-import Login from "./components/login/Login";
+import Login from "./components/Login/Login";
 import ProfilPage from "./components/Profile/Profil";
 import Register from "./components/Register/Register";
 import Footer from "./components/layouts/Footer";
 
 function App() {
-   const router = createBrowserRouter([
-      {
-         path: "/",
-         element: <Home />,
-      },
-      {
-         path: "/PlayStation",
-         element: <PlayStation />,
-      },
-      {
-         path: "/Xbox",
-         element: <Xbox />,
-      },
-      {
-         path: "/Switch",
-         element: <Switch />,
-      },
-      {
-         path: "/Computer",
-         element: <Computer />,
-      },
-      {
-         path: "/ProfilPage",
-         element: <ProfilPage />,
-      },
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/PlayStation",
+      element: <PlayStation />,
+    },
+    {
+      path: "/Xbox",
+      element: <Xbox />,
+    },
+    {
+      path: "/Switch",
+      element: <Switch />,
+    },
+    {
+      path: "/Computer",
+      element: <Computer />,
+    },
+    {
+      path: "/ProfilPage",
+      element: <ProfilPage />,
+    },
 
-      {
-         path: "/Register",
-         element: <Register />,
-      },
-      {
-         path: "/Login",
-         element: <Login />,
-      },
-   ]);
-   return (
-      <>
-         <RouterProvider router={router} />
-         <Footer />
-      </>
-   );
+    {
+      path: "/Register",
+      element: <Register />,
+    },
+    {
+      path: "/Login",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
